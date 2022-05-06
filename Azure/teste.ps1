dc7cc08a6278cf07a5dada7fdb9710357829e43b
Rodrigo Carvalho <rodrigo@rodbr.com>
2022-05-06 19:01:18 -0400

    Azure backup script v1.0

--- Azure/teste.ps1
+++ Azure/teste.ps1
@@ -1,3 +1,6 @@
+# List all Subscriptions:
+# Get-AzSubscription
+
 
 #Get today's date
 $today = (get-date).ToString("yyyyMMdd")
@@ -8,51 +11,13 @@ $weekday = (get-date).DayOfWeek.value__.ToString()
 #Getting the day of Month
 $monthday = (get-date).ToString("dd").PadLeft(2,'0')
 
-# Getting the tags
-$backup_daily=7
-$backup_weeky=2
-$backup_monthly=7
-
-# Defining the day that backups will be performerd
-$week_day_of_backup = 6
-$monthly_day_of_backup = 1
-
-$backup_retention_date=0
-$name_tag=""
-
-#Defining the backup Strategy
-if ($backup_daily -gt 0) {
-    $retention=$backup_daily
-    $name_tag="Daily"
-}
-
-if (($backup_weeky -gt 0) -and ( $weekday -eq $week_day_of_backup)) {
-    $retention=7*$backup_weeky
-    $name_tag="Weekly"
-    
-}
-
-$monthly_day_of_backup = $monthly_day_of_backup.ToString().PadLeft(2,'0')
-
-if (($backup_monthly -gt 0) -and ( $monthday -eq $monthly_day_of_backup)) {
-    $retention=31*$backup_monthly
-    $name_tag="Monthly"
-    
-}
-
-# Showing the backup that will be executed
-$backup_retention_date = (get-date).AddDays($retention).ToString("yyyyMMdd")
-echo "Backup Type: $name_tag"
-echo "Retention: $retention days"
-echo "Date of exclusion: $backup_retention_date"
-
 
 #Connecting on Azure
 $connectionName = "AzureRunAsConnection"
 try{
 	#Getting the service principal connection "AzureRunAsConnection"
 	$servicePrincipalConnection = Get-AutomationConnection -name $connectionName
-	"Logging into Azure..."
+	Write-Host "Logging into Azure..."
 	Add-AzAccount -ServicePrincipal -Tenant $servicePrincipalConnection.TenantID -ApplicationID $servicePrincipalConnection.ApplicationID -CertificateThumbprint $servicePrincipalConnection.CertificateThumbprint
 }
 catch{
@@ -70,3 +35,84 @@ if($err) {
 	throw $err
 }
 
+
+#Get all Vms:
+$machine_list=(Get-AzResource -ResourceType Microsoft.Compute/virtualMachines)
+
+#Getting the machine information
+foreach ($machine in $machine_list )  {
+	$backup = $false
+	Write-Host ""
+	Write-Host "------------------------"
+	$machine_name = $machine.Name
+	$location = $machine.Location
+	$resourceGroupName = $machine.ResourceGroupName
+	Write-Host "Machine ID = $machine.id"
+	Write-Host "Machine Name = $machine_name"
+	Write-Host "Resource Group = $resourceGroupName"
+	Write-Host "------------------------"
+
+	foreach ( $magine_tag in $machine.Tags ) {
+		$name_tag = ""
+		$retention = ""
+		$backup_retention_date = ""
+
+		if ($magine_tag['Backup-Daily']) {
+			$backup = $true
+			$retention=[int]$magine_tag['Backup-Daily']
+    		$name_tag="Daily"
+		}
+		if ( ($magine_tag['Backup-Weekly']) -and ( $weekday -eq $week_day_of_backup)) {
+			$backup = $true
+			$retention=[int]$magine_tag['Backup-Weekly']*7
+    		$name_tag="Weekly"
+		}
+
+		#first Saturday of a Month
+		$date = Get-Date
+		if ( ($magine_tag['Backup-Monthly']) -and ($date.Day -lt 7) -and ($date.DayOfWeek -eq "Friday") ) {
+			$backup = $true
+			$retention=[int]$magine_tag['Backup-Monthly']*31
+    		$name_tag="Monthly"
+		}
+	}
+	$expiration_date = (get-date).AddDays($retention).ToString("yyyyMMdd")
+	if ( $backup ) {
+		Write-Host "Backup Details:"
+		Write-Host "  - Backup Type: $name_tag"
+		Write-Host "  - Retention: $retention days"
+		Write-Host "  - Date of exclusion: $backup_retention_date"
+		$prefix = "snap-"
+		$timestamp = Get-Date -f yyyyMMdd-HHmmss
+		$timestampseparator = "-"
+		$tagname = @{Expiration="$expiration_date";Name="$prefix";Description="$prefix + snapshot";Department="RMG-IT";Admin="Rodrigo Carvalho";Owner="Rodrigo Sakakibara"}
+		$snapshotName = $prefix + $machine_name + $timestampseparator + $timestamp
+		$vm = Get-AzVM -ResourceGroupName $resourceGroupName -Name $machine_name
+
+		# Performing backup from the primary disks
+		Write-Host "  - Snapstot Name: $snapshotName"
+		$snapshot = New-AzSnapshotConfig -Tag $tagname -SourceUri $vm.StorageProfile.OsDisk.ManagedDisk.Id -Location $location -CreateOption copy
+		New-AzSnapshot -Snapshot $snapshot -SnapshotName $snapshotName -ResourceGroupName $resourceGroupName
+
+		if($vm.StorageProfile.DataDisks.Count -ge 1){
+			Write-Host "  - Aditional Disks:"
+			#Condition with more than one data disks
+			for($i=0; $i -le $vm.StorageProfile.DataDisks.Count - 1; $i++){
+
+				#Snapshot name of the data disk
+				$snapshotName = $prefix + $vm.StorageProfile.DataDisks[$i].Name + $timestampseparator + $timestamp
+
+				#Create snapshot configuration
+				$snapshot = New-AzSnapshotConfig -Tag $tagname -SourceUri $vm.StorageProfile.DataDisks[$i].ManagedDisk.Id -Location $location -CreateOption copy
+
+				#Taking the snapshot of the aditional Disk
+				Write-Host "     * Snapstot Name: $snapshotName"
+				New-AzSnapshot -Snapshot $snapshot -SnapshotName $snapshotName -ResourceGroupName $resourceGroupName
+			}
+		}
+
+	} else {
+		Write-Host "** No Backup Defined **"
+	}
+	
+}
